11936857222d219edba9cbd273dce26c05317fcd
Kelly Lima <keely06@live.com>
2021-06-05 11:43:48 -0300

    create all tables empty with sqlite

--- src/database/migrations/02_create_point_items.ts
+++ src/database/migrations/02_create_point_items.ts
@@ -2,21 +2,23 @@ import knex, { Knex } from 'knex'
 
 export async function up(knex: Knex) {
     //  CRIAR A TABELA
-    return knex.schema.createTable('points', table => {
+    return knex.schema.createTable('point_items', table => {
         table.increments('id').primary();
-        table.string('image').notNullable();
-        table.string('name').notNullable();
-        table.string('email').notNullable();
-        table.string('whatsapp').notNullable();
-        table.string('city').notNullable();
-        table.string('uf', 2).notNullable();
-        table.decimal('latitude').notNullable();
-        table.decimal('longitude').notNullable();
+
+        table.integer('point_id')
+        .notNullable()
+        .references('id')
+        .inTable('points');
+        
+        table.integer('item_id')
+        .notNullable()
+        .references('id')
+        .inTable('items');
     })
 }
 
 export async function down(knex: Knex) {
     //VOLTAR ATRÁS (DELETAR A TABELA)
-    return knex.schema.dropTable('point')
+    return knex.schema.dropTable('point_items')
 
 }
