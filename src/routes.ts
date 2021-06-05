669a4ee3076b17e5937bf1d23ec46ae48e2bc90f
Kelly Lima <keely06@live.com>
2021-06-05 15:08:04 -0300

    add serialized items img

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    response.json({
-        message: 'Hello World'
+routes.get('/items', async (request, response) => {
+    //SELEC * FROM items
+    const items = await knex('items').select('*');
+
+    const serializaedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
     })
+
+    return response.json(serializaedItems);
 });
 
 export default routes;
