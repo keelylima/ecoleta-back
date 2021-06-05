4e2a8174d0464ddad788c619795451f4a4bacc1a
Kelly Lima <keely06@live.com>
2021-06-05 18:05:21 -0300

    add transaction knex and point_items

--- src/database/migrations/00_create_points.ts
+++ src/database/migrations/00_create_points.ts
@@ -8,10 +8,10 @@ export async function up(knex: Knex) {
         table.string('name').notNullable();
         table.string('email').notNullable();
         table.string('whatsapp').notNullable();
-        table.string('city').notNullable();
-        table.string('uf', 2).notNullable();
         table.decimal('latitude').notNullable();
         table.decimal('longitude').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
     })
 }
 
